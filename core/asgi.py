ec24eac930ab56ca55ade774e6d11e8e9088a6ba
kanbs <Kartiknagare3165@gmail.com>
2026-06-27 00:35:28 +0530

    done

--- core/asgi.py
+++ core/asgi.py
@@ -1,26 +1,18 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import sensor_app.routing  # 👈 Make sure this file exists
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import sensor_app.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             sensor_app.routing.websocket_urlpatterns
         )
     ),
 })
-
--- sensor_app/routing.py
+++ sensor_app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from sensor_app import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/sensors/$', consumers.SensorConsumer.as_asgi()),
+    re_path(r'^ws/sensors/$', consumers.SensorConsumer.as_asgi()),
 ]
